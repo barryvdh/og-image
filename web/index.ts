1bfd4600be6e25e8a39c0db2090b989bae3a34dd
Barry vd. Heuvel <barry@fruitcake.nl>
2022-03-29 18:56:05 +0200

    Update logos

--- web/index.ts
+++ web/index.ts
@@ -145,16 +145,16 @@ const markdownOptions: DropdownOption[] = [
 ];
 
 const imageLightOptions: DropdownOption[] = [
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'Fruitcake Mark', value: 'https://fruitcake.nl/sites/default/files/fc_rgb_mark_light.svg' },
+    { text: 'Fruitcake Logo', value: 'https://fruitcake.nl/sites/default/files/fc_rgb_logo_light.svg' },
+    { text: 'Fruitcake Logo + Slogan', value: 'https://fruitcake.nl/sites/default/files/fc_rgb_logo_slogan_light.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
 
-    { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
-    { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
-    { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'Fruitcake Mark', value: 'https://fruitcake.nl/sites/default/files/fc_rgb_mark_light.svg' },
+    { text: 'Fruitcake Logo', value: 'https://fruitcake.nl/sites/default/files/fc_rgb_logo_light.svg' },
+    { text: 'Fruitcake Logo + Slogan', value: 'https://fruitcake.nl/sites/default/files/fc_rgb_logo_slogan_light.svg' },
 ];
 
 
